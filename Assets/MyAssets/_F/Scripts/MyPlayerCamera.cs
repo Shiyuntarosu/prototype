d4b22c36b9a943b82a3b3ad2ba7951605dab0a9c
Shiyun <Shiyuntarosu@gmail.com>
2025-09-08 09:52:15 +0900

    手持ちライトの実装

--- Assets/MyAssets/_F/Scripts/MyPlayerCamera.cs
+++ Assets/MyAssets/_F/Scripts/MyPlayerCamera.cs
@@ -7,10 +7,19 @@ public class MyPlayerCamera : MonoBehaviour
     private GameObject _player;
     private CharacterController _controller;
 
+    [Header("camera")]
+    [Tooltip("揺れの大きさ")]
     public float amplitudeGainRatio = 1.0f;
+    [Tooltip("揺れの速さ")]
     public float frequencyGainRatio = 1.0f;
     private float originalAmplitudeGain;
     private float originalFrequencyGain;
+
+
+    [Header("light")]
+    private Light handyLight;
+    [Tooltip("スマホライトのON/OFF")]
+    public bool OnHandyLight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,13 +29,36 @@ public class MyPlayerCamera : MonoBehaviour
         _controller = _player.GetComponent<CharacterController>();
         originalAmplitudeGain = _cinemachineBasicMultiChannelPerlin.AmplitudeGain;
         originalFrequencyGain = _cinemachineBasicMultiChannelPerlin.FrequencyGain;
+
+        handyLight = _player.transform.Find("HandyLight").GetComponent<Light>();
     }
 
     void Update()
     {
+        OnWalkUpdate();
+        OnHandyLightUpdate();
+    }
+
+    // 移動速度に応じてカメラを揺らす
+    private void OnWalkUpdate()
+    {
         // a reference to the players current horizontal velocity
         float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
         _cinemachineBasicMultiChannelPerlin.AmplitudeGain = originalAmplitudeGain + (currentHorizontalSpeed * amplitudeGainRatio);
-        _cinemachineBasicMultiChannelPerlin.FrequencyGain = originalFrequencyGain + (currentHorizontalSpeed * frequencyGainRatio);;
+        _cinemachineBasicMultiChannelPerlin.FrequencyGain = originalFrequencyGain + (currentHorizontalSpeed * frequencyGainRatio); ;
+    }
+
+    // スマホのライト
+    private void OnHandyLightUpdate()
+    {
+        if (OnHandyLight)
+        {
+            handyLight.intensity = 20.0f;
+        }
+        else
+        {
+            handyLight.intensity = 0.0f;
+        }
+
     }
 }
